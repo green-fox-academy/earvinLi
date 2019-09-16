00a361a0f443e44ca4026911fa0ed7b5aa8eeb1f
Earvin Li <earvin.tli@gmail.com>
2019-09-16 18:04:04 +0800

    [W1D1] Finish 05-two-numbers

---
+++ epam-academy/earvinLi/week-01/day-01/05-two-numbers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const a = 22;
+const b = 13;
+
+console.log(`13 added to 22 is ${b + a}.`);
+console.log(`13 substracted from 22 is ${b - a}.`);
+console.log(`22 mutiplied by 13 is ${a * b}.`);
+console.log(`22 divided by 13 is ${(a / b).toFixed(2)}.`);
+console.log(`reminder of 22 divided by 13 is ${a % b}.`);
