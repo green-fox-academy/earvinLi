99ffe5c0ef99e6d6ac1e6552dafbfe67f1018fb7
Earvin Li <earvin.tli@gmail.com>
2019-09-17 17:13:58 +0800

    [W1D2] Finish 29-diagonal-matrix

---
+++ epam-academy/earvinLi/week-01/day-02/29-diagonal-matrix.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+const diagonalMatrix = lines => {
+  let targetMatrix = [];
+
+  for (let i = 0; i < lines; i++) {
+    let tempLine = [];
+
+    for (let j = 0; j < lines; j++) {
+      if (j === lines - i - 1) {
+        tempLine.push(1);
+      } else {
+        tempLine.push(0);
+      }
+    }
+    
+    targetMatrix.push(tempLine);
+  }
+  return targetMatrix;
+}
+
+console.log(diagonalMatrix(4).forEach(line => console.log(line)));
