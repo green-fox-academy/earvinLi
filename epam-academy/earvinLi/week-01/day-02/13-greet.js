962704bec398a0ed2cd04c19af2a5f7f22316e2b
Earvin Li <earvin.tli@gmail.com>
2019-09-17 15:14:57 +0800

    [W1D2] Finish 13-greet

---
+++ epam-academy/earvinLi/week-01/day-02/13-greet.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// - Create variable named `al` and assign the value `EPAM` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear EPAM`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+const ai = 'EPAM';
+
+const greet = (client = 'Earvin') => {
+  console.log(`Greetings, dear ${client}.`);
+};
+
+greet(ai);
