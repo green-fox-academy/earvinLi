49b7dd665fc7250f83ddc0b045c177fc880854e1
Earvin Li <earvin.tli@gmail.com>
2019-09-17 13:43:05 +0800

    [W1D2] Finish 04-multiplication-table

---
+++ epam-academy/earvinLi/week-01/day-02/04-multiplication-table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const number = 15;
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+const multiplicationTable = num => {
+  for (let i = 1; i < 11; i++) console.log(`${i} * ${num} = ${i * num}`);
+};
+
+multiplicationTable(number);
