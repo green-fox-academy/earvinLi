3eac20d162bcbe1fa2f786241179776f2c1b3987
Earvin Li <earvin.tli@gmail.com>
2019-09-17 15:18:32 +0800

    [W1D2] Finish 14-append-s

---
+++ epam-academy/earvinLi/week-01/day-02/14-append-s.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// - Create a variable named `am` and assign the value `dog` to it
+// - Write a function called `appendS` that gets a string as an input
+//   and appends an 's' character to its end
+// - Print the result of `appendS(am)` to the console
+const am = 'dog';
+
+const appendS = str => {
+  return `${str}s`;
+};
+
+console.log(appendS(am));
