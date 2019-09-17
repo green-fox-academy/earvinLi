00dafef6d4d0e1f6d277fe9f22400e79ebe6500a
Earvin Li <earvin.tli@gmail.com>
2019-09-17 16:19:44 +0800

    [W1D2] Finish 23-change-element

---
+++ epam-academy/earvinLi/week-01/day-02/23-change-element.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method
+// - Print the fourth element as a test
+const s = [1, 2, 3, 8, 5, 6];
+
+const newS = s.map(num => {
+  if (num === 8) num = 4;
+  return num;
+});
+
+console.log(newS);
