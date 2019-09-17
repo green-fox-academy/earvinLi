39ce50d7cb16f23b45ffb68ecaa26b22aa88eec0
Earvin Li <earvin.tli@gmail.com>
2019-09-17 10:20:04 +0800

    [W1D1] Finish 10-define-basic-info

---
+++ epam-academy/earvinLi/week-01/day-01/10-define-basic-info.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const name = 'Earvin';
+const age = 27;
+const height = 1.72;
+const maritalStatus = false;
+
+console.log(`My name is ${name}. I'm ${age} years old and ${height} meters tall. I'm ${'not' || maritalStatus} married.`);
