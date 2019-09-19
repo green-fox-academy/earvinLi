0700384fa02c24725352a13141a743226a85d9f6
Earvin Li <earvin.tli@gmail.com>
2019-09-19 12:29:41 +0800

    [W1D4] Finish 03-animal

---
+++ epam-academy/earvinLi/week-01/day-04/03-animal.js
@@ -0,0 +1,26 @@
+class Animal {
+  constructor(hunger = 50, thirst = 50) {
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
+
+  eat() { this.hunger-- }
+  drink() { this.thirst-- }
+  play() {
+    this.hunger++;
+    this.thirst++;
+  }
+}
+
+const animalOne = new Animal();
+
+console.log(animalOne.hunger);
+
+animalOne.eat();
+
+console.log(animalOne.hunger);
+
+animalOne.drink();
+animalOne.play();
+
+console.log(animalOne);
