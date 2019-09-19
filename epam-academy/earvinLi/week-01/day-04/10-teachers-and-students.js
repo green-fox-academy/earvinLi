8f7f334f1538d76fdf1c9d555e4b7b5e237565f0
Earvin Li <earvin.tli@gmail.com>
2019-09-19 15:58:35 +0800

    [W1D4] Finish 10-teachers-and-students

---
+++ epam-academy/earvinLi/week-01/day-04/10-teachers-and-students.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Student {
+  constructor(subject) { this.subject = subject };
+
+  learn() { console.log(`I'm studying ${this.subject}.`); }
+
+  question(teacher) { teacher.answer(this.subject); }
+}
+
+class Teacher {
+  teach(student) { student.learn(); };
+
+  answer(subject) { console.log(`I'm answering my student's ${subject} question.`); }
+}
+
+const studentOne = new Student('Math');
+const teacherOne = new Teacher();
+
+studentOne.question(teacherOne);
+teacherOne.teach(studentOne);
