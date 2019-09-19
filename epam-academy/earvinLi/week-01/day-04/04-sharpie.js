0146ee503c979e232a95cb8ef471b21ce6787588
Earvin Li <earvin.tli@gmail.com>
2019-09-19 12:35:54 +0800

    [W1D4] Finish 04-sharpie

---
+++ epam-academy/earvinLi/week-01/day-04/04-sharpie.js
@@ -0,0 +1,13 @@
+class Sharpie {
+  constructor(color, width, inkAmount = 100.00) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = inkAmount;
+  }
+
+  use() { this.inkAmount-- }
+}
+
+const sharpieOne = new Sharpie('black', 1.5);
+sharpieOne.use();
+console.log(sharpieOne.inkAmount);
