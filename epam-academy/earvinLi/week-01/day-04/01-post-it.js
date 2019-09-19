aeab88188d4f24cd6744a60dc3dd6e96809aad05
Earvin Li <earvin.tli@gmail.com>
2019-09-19 11:36:30 +0800

    [W1D4] Finish 01-post-it

---
+++ epam-academy/earvinLi/week-01/day-04/01-post-it.js
@@ -0,0 +1,13 @@
+class PostIt {
+  constructor(backgroundColor, text, textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
+
+const postItOne = new PostIt('orange', 'blue', 'Idea 1');
+const postItTwo = new PostIt('pink', 'black', 'Awesome');
+const postItThree = new PostIt('yellow', 'green', 'Superb!');
+
+console.log(postItOne.text);
