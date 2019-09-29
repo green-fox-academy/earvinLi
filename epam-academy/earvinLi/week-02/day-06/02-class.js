c41187b88df30b108dc422b82b29825ab2a83570
Earvin Li <earvin.tli@gmail.com>
2019-09-29 15:35:11 +0800

    [W2D6] Finish 02-class

---
+++ epam-academy/earvinLi/week-02/day-06/02-class.js
@@ -0,0 +1,43 @@
+// ES5
+function GardenES5(width, length) {
+  this.width = width;
+  this.length = length;
+}
+
+GardenES5.prototype.area = function() {
+  return this.width * this.length;
+};
+
+GardenES5.prototype.circumference = function() {
+  return this.width * 2 + this.length * 2;
+};
+
+GardenES5.prototype.efficiency = function() {
+  return this.area() / this.circumference();
+};
+
+var gardenOne = new GardenES5(4, 5);
+console.log(gardenOne.efficiency());
+
+// ES6
+class GardenES6 {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+
+  area() {
+    return this.width * this.length;
+  }
+
+  circumference() {
+    return this.width * 2 + this.length * 2;
+  }
+
+  efficiency() {
+    return this.area() / this.circumference();
+  }
+}
+
+const gardenTwo = new GardenES6(4, 5);
+console.log(gardenTwo.efficiency());
