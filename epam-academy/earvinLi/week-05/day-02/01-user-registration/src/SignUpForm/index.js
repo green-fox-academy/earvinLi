360d8f062caac2ec3f88c3d953b7c966b603185c
Earvin Li <earvin.tli@gmail.com>
2019-10-22 17:27:34 +0800

    [W5D2] [User Registration] Add validations (without button disablings)

--- epam-academy/earvinLi/week-05/day-02/01-user-registration/src/SignUpForm/index.js
+++ epam-academy/earvinLi/week-05/day-02/01-user-registration/src/SignUpForm/index.js
@@ -27,8 +27,8 @@ function SignUpForm(props) {
   const [inputValues, setInputValues] = useState({
     firstName: '',
     lastName: '',
-    username: '',
-    email: '',
+    username: 'iEarvin',
+    email: 'earvin.tli@gmail.com',
     password: '',
   });
 
@@ -45,41 +45,52 @@ function SignUpForm(props) {
   });
 
   const textFieldSection = [
-    { id: 'first-name', label: 'First Name', name: 'firstName', onChange: onChangeInputValue },
-    { id: 'last-name', label: 'Last Name', name: 'lastName', onChange: onChangeInputValue },
-    { id: 'username', label: 'Userame', name: 'username', onChange: onChangeInputValue },
-    { id: 'email', label: 'Email', name: 'email', onChange: onChangeInputValue },
-    { id: 'password', label: 'Password', name: 'password', onChange: onChangeInputValue },
-  ].map((textField, index) => {
+    { id: 'first-name', label: 'First Name', name: 'firstName', value: inputValues.firstName },
+    { id: 'last-name', label: 'Last Name', name: 'lastName', value: inputValues.lastName },
+    { id: 'username', label: 'Userame', name: 'username', value: inputValues.username },
+    { id: 'email', label: 'Email', name: 'email', value: inputValues.email },
+    { id: 'password', label: 'Password', name: 'password', value: inputValues.password },
+  ].map((textField, textFieldIndex) => {
     const {
       id,
       label,
       name,
-      onChange,
+      value,
     } = textField
 
-    const type = (index === 3 || index === 4) ? name : '';
+    const type = (textFieldIndex === 3 || textFieldIndex === 4) ? name : '';
 
     return (
       <TextField
         className={textFieldStyles}
         id={id}
+        inputProps={textFieldIndex === 4 ? { minLength: 8 } : {}}
         key={id}
         label={label}
         margin="normal"
         name={name}
-        onChange={onChange}
+        onChange={onChangeInputValue}
+        required={textFieldIndex > 1 }
         type={type}
+        value={value}
       />
     );
   });
 
   return (
-    <form autoComplete="off" className={formStyles}>
+    <form
+      autoComplete="off"
+      className={formStyles}
+      onSubmit={(event) => {
+        onSignUpUser(inputValues);
+        event.preventDefault();
+      }}
+    >
       {textFieldSection}
+      {/* TODO: Disable the button according to the validity status of the form */}
       <Button
         color="primary"
-        onClick={() => onSignUpUser(inputValues)}
+        type="submit"
         variant="contained"
       >
         SIGN UP
