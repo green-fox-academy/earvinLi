c7d0330c233cbe9f643703339810727dbc543252
Earvin Li <earvin.tli@gmail.com>
2019-10-25 12:02:24 +0800

    [W5D5] [Rest Backend Practices] Finish greeter endpoint

--- epam-academy/earvinLi/week-05/day-05/01-rest-backend-practices/server.js
+++ epam-academy/earvinLi/week-05/day-05/01-rest-backend-practices/server.js
@@ -3,11 +3,27 @@ const path = require('path');
 const app = require('./app');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// Doubling endpoint
 app.get('/doubling', (req, res) => {
   const { input } = req.query;
-  if (!input) res.send({ error: 'Please provide an input!' })
+  if (!input) res.send({ error: 'Please provide an input!' });
   res.send({
     'received': input,
     'result': input * 2
   });
 })
+
+// Greeter endpoint
+app.get('/greeter', (req, res) => {
+  const {
+    name,
+    title,
+  } = req.query;
+
+  if (!name && !title) res.send({ error: 'Please provide a name and a title!' });
+  if (!name) res.send({ error: 'Please provide a name!' });
+  if (!title) res.send({ error: 'Please provide a title!' });
+
+  res.send({ welcome_message: `Oh, hi there ${name}, my dear ${title}!` });
+});
