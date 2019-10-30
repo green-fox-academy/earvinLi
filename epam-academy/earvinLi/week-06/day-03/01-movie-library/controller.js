56b76ff6cd8449564430ef0c748277866b31662c
Earvin Li <earvin.tli@gmail.com>
2019-10-30 17:54:17 +0800

    [W6D2] [Movie Library] Finish delete and post endpoints

--- epam-academy/earvinLi/week-06/day-03/01-movie-library/controller.js
+++ epam-academy/earvinLi/week-06/day-03/01-movie-library/controller.js
@@ -1,22 +1,41 @@
 const http = require('http');
 const url = require('url');
 
+const getParam = (pathname) => {
+  const pathnameElements = pathname.split('/');
+  const param = pathnameElements[pathnameElements.length - 1];
+  return param;
+};
+
 module.exports = http.createServer((req, res) => {
   const service = require('./service.js');
   const reqUrl = url.parse(req.url, true);
 
-  // GET Endpoint
+  // Movies Get Endpoint
   if (req.method === 'GET') {
-    if (reqUrl.pathname === '/movies') return service.moviesRequest(req, res);
-    else {
-      const pathnameElements = reqUrl.pathname.split('/');
-      const movieId = pathnameElements[pathnameElements.length - 1];
+    if (reqUrl.pathname === '/movies') {
+      return service.moviesGetRequest(req, res);
+    } else {
+      const movieId = getParam(reqUrl.pathname);
+
       if (!/^\d+$/.test(movieId)) return service.invalidRequest(req, res);
-      return service.moviesRequest(req, res, parseInt(movieId));
+      return service.moviesGetRequest(req, res, parseInt(movieId));
     }
   }
-  else if (reqUrl.pathname === '/test' && req.method === 'POST' ) {
-    service.testRequest(req, res);
+  // Movies Delete Endpoint
+  else if (req.method === 'DELETE' ) {
+    if (reqUrl.pathname === '/movies') {
+      return service.moviesDeleteRequest(req, res);
+    } else {
+      const movieId = getParam(reqUrl.pathname);
+
+      if (!/^\d+$/.test(movieId)) return service.invalidRequest(req, res);
+      return service.moviesDeleteRequest(req, res, parseInt(movieId));
+    }
+  }
+  // Movies Post Endpoint
+  else if (reqUrl.pathname === '/movies' && req.method === 'POST' ) {
+    service.moviesPostRequest(req, res);
   }
   else {
     service.invalidRequest(req, res);
--- epam-academy/earvinLi/week-06/day-03/01-movie-library/service.js
+++ epam-academy/earvinLi/week-06/day-03/01-movie-library/service.js
@@ -1,28 +1,64 @@
 const url = require('url');
 
-exports.moviesRequest = (req, res, param) => {
+exports.moviesGetRequest = (req, res, param) => {
   const reqUrl = url.parse(req.url, true);
   let response = movieData;
 
-  if (reqUrl.query.genre) {
-    response = response.filter(item => item.genre.includes(reqUrl.query.genre));
-  }
-
-  if (param) {
-    response = response.filter(item => item.id === parseInt(param));
-  }
+  if (reqUrl.query.genre) response = response.filter(item => item.genre.includes(reqUrl.query.genre));
+  if (param) response = response.filter(item => item.id === param);
 
   res.statusCode = 200;
   res.setHeader('Content-type', 'application/json');
   res.end(JSON.stringify(response));
 };
 
-exports.testRequest = (req, res) => {
+exports.moviesDeleteRequest = (req, res, param) => {
+  const reqUrl = url.parse(req.url, true);
+
+  if (!req.headers.authorization || req.headers.authorization !== 'top-secret') {
+    res.statusCode = 403;
+    // TODO: Check the return
+    res.end();
+  }
+
+  if (param) {
+    if (!movieData.find(item => item.id === param)) return res.statusCode = 204;
+    movieData = movieData.filter(item => item.id !== param);
+    res.statusCode = 204;
+  }
+  else {
+    movieData = [];
+    res.statusCode = 204;
+  }
+  res.end();
+};
+
+exports.moviesPostRequest = (req, res) => {
+  if (!req.headers.authorization || req.headers.authorization !== 'top-secret') {
+    res.statusCode = 403;
+    // TODO: Check the return
+    res.end();
+  }
+
   let body = '';
   req.on('data', chunk => body += chunk);
   req.on('end', () => {
     const postBody = JSON.parse(body);
-    const response = { "text": `Post Request Value is ${postBody.value}` };
+
+    if (!postBody.hasOwnProperty('title')) {
+      res.statusCode = 400;
+      // TODO: Check the return
+      return res.end();
+    }
+    if (movieData.find(item => item.title === postBody.title)) {
+      res.statusCode = 409;
+      // TODO: Check the return
+      return res.end();
+    }
+    if (!postBody.hasOwnProperty('genre')) postBody['genre'] = 'unknown';
+
+    movieData.push(postBody);
+    const response = { "text": `Post Request Value is ${postBody.genre}` };
 
     res.statusCode = 200;
     res.setHeader('Content-type', 'application/json');
@@ -36,7 +72,7 @@ exports.invalidRequest = (req, res) => {
   res.end('Invalid Request');
 };
 
-const movieData = [
+let movieData = [
   { id: 1, title: 'New World', genre: ['thriller', 'drama'] },
   { id: 2, title: 'Cloud Atlas', genre: ['drama', 'fantasy'] },
   { id: 3, title: 'The Final Master', genre: ['drama', 'action'] },
