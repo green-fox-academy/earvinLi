fd668d3e92e1a99fb4bb3945e2164d6cc10377b1
Earvin Li <earvin.tli@gmail.com>
2019-11-05 18:40:56 +0800

    [W7D1] [Reddit Mockup] Add static PostAddDialog

--- epam-academy/earvinLi/week-07/01-reddit-mockup/src/Sidebar/index.js
+++ epam-academy/earvinLi/week-07/01-reddit-mockup/src/Sidebar/index.js
@@ -1,5 +1,5 @@
 // External Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 
 // Material-UI Dependencies
 import Button from '@material-ui/core/Button';
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 
 // Internal Dependencies
+import PostAddDialog from './PostAddDialog';
 import { getSidebarStyles } from '../App/Styles';
 
 // Local Variables
@@ -19,16 +20,25 @@ const Sidebar = () => {
     rootStyle,
   } = useStyles();
 
+  const [ isOpen, setIsOpen ] = useState(false);
+
   return (
-    <Paper className={rootStyle}>
-      <Button
-        className={buttonStyle}
-        color="primary"
-        variant="contained"
-      >
-      SUBMIT A NEW POST
-    </Button>
-    </Paper>
+    <>
+      <Paper className={rootStyle}>
+        <Button
+          className={buttonStyle}
+          color="primary"
+          onClick={() => setIsOpen(true)}
+          variant="contained"
+        >
+        SUBMIT A NEW POST
+      </Button>
+      </Paper>
+      <PostAddDialog
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    </>
   );
 };
 
