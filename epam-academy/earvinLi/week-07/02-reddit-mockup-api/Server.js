03f98eab5822de3451e6e78d98bca6be8232ec68
Earvin Li <earvin.tli@gmail.com>
2019-11-05 16:56:19 +0800

    [W7D2] [Reddit Mockup API] Add cors middleware

--- epam-academy/earvinLi/week-07/02-reddit-mockup-api/Server.js
+++ epam-academy/earvinLi/week-07/02-reddit-mockup-api/Server.js
@@ -1,13 +1,21 @@
 // External Dependencies
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 
 // Internal Dependencies
 const PostsRouter = require('./PostsRouter');
 
+// Local Variables
 const PORT = process.env.PORT;
-const Server = express();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
 
+// Server Configuration
+const Server = express();
+Server.use(cors(corsOptions));
 Server.use(express.json());
 Server.use('/posts', PostsRouter);
+
 Server.listen(PORT, () => console.log(`Server running at ${PORT}.`));
