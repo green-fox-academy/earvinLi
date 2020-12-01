0dba9966f699ad95ced3e565caa5dc05f0c9d91c
Earvin <earvin.tli@gmail.com>
2020-12-01 18:41:46 +0800

    [JMA] [W1D2] Finish exercises 01-07

---
+++ java-microservice-academy/week-01/day-02/03-hello-others.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    System.out.println("Hello, Martin!");
+    System.out.println("Hello, Jereld!");
+    System.out.println("Hello, Ramapriyan!");
+  }
+}
